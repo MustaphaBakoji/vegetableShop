d634cf127ed4a6a90afc2311da3d0f98d55f2a95
bakoji <mustafabakoji@gmail.com>
2024-07-22 16:06:28 +0100

    merged

--- server/index.js
+++ server/index.js
@@ -11,3 +11,48 @@ Cloudinary.config({
     api_key: process.env.CLOUDINARY_SECRET,
     api_secret: process.env.CLOUDINARY_SECRET
 })
+
+app.use(session({
+    cookie: { name: "user", secret: process.env.SECRET_KEY },
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET_KEY
+}))
+
+mongoose.connect('')
+    .then(() => {
+        console.log('connected to db')
+    }).catch((err) => {
+        console.log(err)
+    });
+
+const PORT = 4000 || process.env.PORT
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('cors')({ origin: "*" }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser((user, done) => {
+    done(null, { id: user.id, type: user.role });
+});
+
+passport.deserializeUser((obj, done) => {
+    if (obj.type === 'user') {
+        User.findById(obj.id, (err, user) => {
+            done(err, user);
+        });
+    } else if (obj.type === 'admin') {
+        Admin.findById(obj.id, (err, admin) => {
+            done(err, admin);
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('Conneted to server');
+})
+
+
