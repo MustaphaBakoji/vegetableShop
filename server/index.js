1dddefbd9a564429a9d9a837a3b3f010571a1752
venemousCoder <ahmadgameriv@gmail.com>
2024-07-22 16:00:07 +0100

    file configuration Co-authored-by: MustaphaBakoji <MustaphaBakoji@users.noreply.github.com>

--- server/index.js
+++ server/index.js
@@ -1,6 +1,58 @@
 const Cloudinary = require("cloudinary").v2
+const express = require('express');
+require('dotenv').config();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const app = express()
+
 Cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_SECRET,
     api_secret: process.env.CLOUDINARY_SECRET
 })
+
+app.use(session({
+    cookie: { name: "user", secret: process.env.SECRET_KEY },
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SECRET_KEY
+}))
+
+mongoose.connect('')
+    .then(() => {
+        console.log('connected to db')
+    }).catch((err) => {
+        console.log(err)
+    });
+
+const PORT = 4000 || process.env.PORT
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require('cors')({ origin: "*" }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser((user, done) => {
+    done(null, { id: user.id, type: user.role });
+  });
+  
+  passport.deserializeUser((obj, done) => {
+    if (obj.type === 'user') {
+      User.findById(obj.id, (err, user) => {
+        done(err, user);
+      });
+    } else if (obj.type === 'admin') {
+      Admin.findById(obj.id, (err, admin) => {
+        done(err, admin);
+      });
+    }
+  });
+
+app.listen(PORT, ()=>{
+    console.log('Conneted to server');
+})
+
+
---
+++ server/models/user.models.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const PassportLocalMongoose = require('passport-local-mongoose');
+const UserScheme = mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+            enum: ['user', 'admin'],
+            default: 'user'
+        },
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+UserScheme.plugin(PassportLocalMongoose)
+
+module.exports = mongoose.model('User', UserScheme);
