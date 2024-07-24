2a0216337288bff2ad20f6eced79e97582166e2b
bakoji <mustafabakoji@gmail.com>
2024-07-24 13:51:17 +0100

    migrate_from_typescript_to_js_for+peace

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-
-export default {
+module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui')
-  ]
+  plugins: [require('daisyui')],
 }
 
